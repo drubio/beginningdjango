e16dc2e0bb4fea47f6963c4871681b5af33d07e5
Daniel <daniel@webforefront.com>
2013-05-05 02:13:44 -0500

    Added new custom filters

--- 3_django_templates/coffeehouse/settings.py
+++ 3_django_templates/coffeehouse/settings.py
@@ -67,7 +67,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/www/djangorecipes/3_django_templates/coffeehouse/assets/',
+    '/www/djangorecipes/3_django_templates/assets/',
 )
 
 # List of finder classes that know how to find static files in
