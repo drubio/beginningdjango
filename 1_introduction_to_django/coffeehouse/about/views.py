b3451ef2905fe06def47fe7d67468f6fa06123ce
Daniel <daniel@webforefront.com>
2014-05-24 18:22:06 -0500

    Updated Recipe for Ch1 with Django 1.7

--- 1_introduction_to_django/coffeehouse/about/views.py
+++ 1_introduction_to_django/coffeehouse/about/views.py
@@ -1,6 +1,5 @@
 # Create your views here.
-from django.shortcuts import render_to_response
-from django.template import RequestContext
+from django.shortcuts import render
 
 def contact(request):
-    return render_to_response('about/contact.html',context_instance=RequestContext(request))
+    return render(request,'about/contact.html')
--- 1_introduction_to_django/coffeehouse/urls.py
+++ 1_introduction_to_django/coffeehouse/urls.py
@@ -1,14 +1,10 @@
 from django.conf.urls import patterns, include, url
-from django.views.generic import TemplateView
-
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
-admin.autodiscover()
-
+from django.views.generic import TemplateView
 
 urlpatterns = patterns('',
     url(r'^$',TemplateView.as_view(template_name='homepage.html')),
     url(r'^about/','coffeehouse.about.views.contact'),
-    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 )
