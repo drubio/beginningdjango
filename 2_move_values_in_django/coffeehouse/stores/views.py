31ebe6a9a1c84b5082f84c3ded73bbd18f3ac436
Daniel <daniel@webforefront.com>
2014-05-25 14:03:08 -0500

    Updated app for Ch 2 recipes to Django 1.7

--- 2_move_values_in_django/coffeehouse/stores/views.py
+++ 2_move_values_in_django/coffeehouse/stores/views.py
@@ -1,6 +1,5 @@
 # Create your views here.
-from django.shortcuts import render_to_response
-from django.template import RequestContext
+from django.shortcuts import render
 
 def index(request,store_id=1,location=''):
     # Access store_id parameter with 'store_id' variable 
@@ -18,4 +17,4 @@ def index(request,store_id=1,location=''):
     store_amenities = ['WiFi','A/C']
     store_menu = ((0,''),(1,'Drinks'),(2,'Food'))
     vals_for_template = {'store_name':STORE_NAME,'store_address':store_address,'store_amenities':store_amenities,'store_menu':store_menu}
-    return render_to_response('stores/index.html', vals_for_template, context_instance=RequestContext(request))
+    return render(request,'stores/index.html', vals_for_template)
--- 2_move_values_in_django/coffeehouse/urls.py
+++ 2_move_values_in_django/coffeehouse/urls.py
@@ -1,9 +1,6 @@
 from django.conf.urls import patterns, include, url
-from django.views.generic import TemplateView
-
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
-admin.autodiscover()
+from django.views.generic import TemplateView
 
 
 urlpatterns = patterns('',
@@ -14,5 +11,4 @@ urlpatterns = patterns('',
     url(r'^stores/','coffeehouse.stores.views.index',{'location':'headquarters'}),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
-
 )
