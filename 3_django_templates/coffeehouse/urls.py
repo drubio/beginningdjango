12f6ebf262eeef2fa2b89d5060aac31921557d2d
Daniel <daniel@webforefront.com>
2014-12-29 13:06:33 -0600

    Updated code for Ch 3 with named links and updated URLs

--- 3_django_templates/coffeehouse/urls.py
+++ 3_django_templates/coffeehouse/urls.py
@@ -8,11 +8,11 @@ admin.autodiscover()
 from coffeehouse.drinks.urls import urlpatterns as drinks_url_patterns
 
 urlpatterns = patterns('',
-    url(r'^$',TemplateView.as_view(template_name='homepage.html'),{'homepage':True,'thejs':'"mocha\r\n \'price:2.25"'}),
+    url(r'^$',TemplateView.as_view(template_name='homepage.html'),name="homepage"),
     url(r'^about/', include('coffeehouse.about.urls',namespace="about")),
     url(r'^drinks/', include(drinks_url_patterns,namespace="drinks")),
     url(r'^stores/',include('coffeehouse.stores.urls',namespace="stores")),
-    url(r'^online/',TemplateView.as_view(template_name='online/index.html')),
+    url(r'^online/',TemplateView.as_view(template_name='online/index.html'),name='online'),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
