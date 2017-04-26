2b3e61768a8667b74b85b264ed0543e8a4bee34e
Daniel <daniel@webforefront.com>
2017-04-26 02:52:16 -0500

    Updated Ch3 sample

--- 3_django_templates/coffeehouse/settings.py
+++ 3_django_templates/coffeehouse/settings.py
@@ -24,7 +24,7 @@ INSTALLED_APPS = (
     'coffeehouse.drinks',
 )
 
-MIDDLEWARE_CLASSES = (
+MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -32,7 +32,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-)
+]
 
 ROOT_URLCONF = 'coffeehouse.urls'
 
