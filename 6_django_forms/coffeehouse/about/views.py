80a8c018c8a04d71444fc396a8773b6166c0ebad
Daniel <daniel@webforefront.com>
2017-04-27 22:18:55 -0500

    Added Ch 6 forms

---
+++ 6_django_forms/coffeehouse/about/views.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+from coffeehouse.stores.views import STORE_LIST
+from django.http import Http404, HttpResponseRedirect
+from .forms import ContactForm
+
+
+def index(request,store_id=None):
+    # Create fixed data structures to pass to template
+    # data could equally come from database queries
+    if store_id == None:
+        store = STORE_LIST[0]
+    elif store_id == "1":
+        store = STORE_LIST[1]
+    elif store_id == "2":
+        store = STORE_LIST[2]
+    elif store_id == "3":
+        store = STORE_LIST[3]
+    else:
+        raise Http404
+    return render(request,'about/index.html',{'store':store})
+
+def contact(request,store_id=None):
+    if request.method == 'POST':
+        # POST, generate form with data from the request
+        form = ContactForm(request.POST)
+        # check if it's valid:
+        if form.is_valid():
+            # process data, insert into DB, generate email,etc
+            # redirect to a new URL:
+            return HttpResponseRedirect('/about/contact/thankyou')
+    else:
+        # GET, generate blank form
+        form = ContactForm(field_order=['email','name','comment'],label_suffix=' : ',initial={'user':request.user,'otherstuff':'otherstuff'},use_required_attribute=False,auto_id=False)
+    return render(request,'about/contact.html',{'form':form})
---
+++ 6_django_forms/coffeehouse/drinks/urls.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.conf.urls import include, url
+from coffeehouse.drinks import views as drinks_views
+
+urlpatterns = [
+    url(r'^$',drinks_views.index,name="index"),
+    url(r'^(?P<drink_type>\D+)/$',drinks_views.detail,name="detail"),
+]
