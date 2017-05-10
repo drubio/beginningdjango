b61da20fdc926e2e3d94e00cf4061c6d438401df
Daniel <daniel@webforefront.com>
2017-05-10 16:24:52 -0500

    Added Py2 support for model classes

--- 7_django_models/coffeehouse/items/models.py
+++ 7_django_models/coffeehouse/items/models.py
@@ -1,13 +1,14 @@
+# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from django.utils.encoding import python_2_unicode_compatible
 
 from django.db import models
 
+@python_2_unicode_compatible
 class Menu(models.Model):
     name = models.CharField(max_length=30)
-    def __unicode__(self):
-        return u"%s" % (self.name)
     def __str__(self):
-        return u"%s" % (self.name)
+        return "%s" % (self.name)
 
 ITEM_SIZES = (
             ('S','Small'),
@@ -16,6 +17,7 @@ ITEM_SIZES = (
             ('P','Portion'),
             )
 
+@python_2_unicode_compatible
 class Item(models.Model):
     menu = models.ForeignKey(Menu, on_delete=models.CASCADE)
     name = models.CharField(max_length=30)
@@ -23,16 +25,13 @@ class Item(models.Model):
     size = models.CharField(choices=ITEM_SIZES,max_length=1)
     calories = models.IntegerField()
     price = models.FloatField(blank=True,null=True)
-    def __unicode__(self):
-        return u"%s (%s)" % (self.name, self.description)
     def __str__(self):
-        return u"%s (%s)" % (self.name, self.description)
+        return "%s (%s)" % (self.name, self.description)
 
-
+    
+@python_2_unicode_compatible
 class Drink(models.Model):
     item = models.OneToOneField(Item,on_delete=models.CASCADE,primary_key=True)
     caffeine = models.IntegerField()
-    def __unicode__(self):
-        return u"%s (%s)" % (self.item, self.caffeine)
     def __str__(self):
-        return u"%s (%s)" % (self.item, self.caffeine)
+        return "%s (%s)" % (self.item, self.caffeine)
