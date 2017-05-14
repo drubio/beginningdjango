d67e0ae499ba9a775385d0fe34902486a5f53de4
Daniel <daniel@webforefront.com>
2017-05-13 21:09:34 -0500

    Added new field

--- 7_django_models/coffeehouse/items/models.py
+++ 7_django_models/coffeehouse/items/models.py
@@ -25,6 +25,7 @@ class Item(models.Model):
     size = models.CharField(choices=ITEM_SIZES,max_length=1)
     calories = models.IntegerField()
     price = models.FloatField(blank=True,null=True)
+    stock = models.IntegerField(default=0)
     def __str__(self):
         return "%s (%s)" % (self.name, self.description)
 
