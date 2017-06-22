0e2dc2cf9210cf6dfda50045f53ba2d53c586f31
Daniel <daniel@webforefront.com>
2017-06-22 01:11:15 -0500

    Added custom model managers

--- 8_and_9_django_model_constructs/coffeehouse/items/models.py
+++ 8_and_9_django_model_constructs/coffeehouse/items/models.py
@@ -6,6 +6,13 @@ from django import forms
 from django.db import models
 
 
+class ItemMenuManager(models.Manager):
+    def salad_items(self):
+        return self.filter(menu__name='Salads')
+                       
+    def sandwich_items(self):
+        return self.filter(menu__name='Sandwiches')    
+    
 @python_2_unicode_compatible
 class Menu(models.Model):
     name = models.CharField(max_length=30)
@@ -27,7 +34,9 @@ class Item(models.Model):
     size = models.CharField(choices=ITEM_SIZES,max_length=1)
     calories = models.IntegerField()
     price = models.FloatField(blank=True,null=True)
-    stock = models.IntegerField(default=0)    
+    stock = models.IntegerField(default=0)
+    objects = models.Manager()    
+    menumgr = ItemMenuManager()
     def __str__(self):
         return "%s (%s)" % (self.name, self.description)
 
--- 8_and_9_django_model_constructs/coffeehouse/stores/models.py
+++ 8_and_9_django_model_constructs/coffeehouse/stores/models.py
@@ -6,6 +6,32 @@ from django.db import models
 
 from django.core.exceptions import ValidationError
 
+class StoreQuerySet(models.QuerySet):
+    def sandiego(self):
+        return self.filter(city='San Diego')
+
+    def losangeles(self):
+        return self.filter(city='Los Angeles')
+
+class StoreManager(models.Manager):
+    def get_queryset(self):
+        return StoreQuerySet(self.model, using=self._db)
+
+    def sandiego(self):
+        return self.get_queryset().sandiego()
+
+    def losangeles(self):
+        return self.get_queryset().losangeles()
+    
+class SanDiegoStoreManager(models.Manager):
+    def get_queryset(self):
+        return super(SanDiegoStoreManager, self).get_queryset().filter(city='San Diego')
+
+class LosAngelesStoreManager(models.Manager):
+    def get_queryset(self):
+        return super(LosAngelesStoreManager, self).get_queryset().filter(city='Los Angeles')    
+
+    
 @python_2_unicode_compatible
 class Amenity(models.Model):
     name = models.CharField(max_length=30)
@@ -21,6 +47,10 @@ class Store(models.Model):
     state = models.CharField(max_length=2)
     email = models.EmailField()
     amenities = models.ManyToManyField(Amenity,blank=True)
+    objects = models.Manager()
+    sandiego = SanDiegoStoreManager()
+    losangeles = LosAngelesStoreManager()
+    shops = StoreQuerySet.as_manager()
     def __str__(self):
         return "%s (%s,%s)" % (self.name, self.city,self.state)
     def clean(self):
