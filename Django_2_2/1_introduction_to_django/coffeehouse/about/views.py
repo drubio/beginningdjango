38e38a9fb0643d48c5583507c6b5bca3b6d2a1cf
Daniel R <daniel@webforefront.com>
2019-04-07 17:41:03 -0500

    Updated Ch 1 with official Django 2.2 release

--- Django_2_2/1_introduction_to_django/coffeehouse/about/views.py
+++ Django_2_2/1_introduction_to_django/coffeehouse/about/views.py
@@ -4,5 +4,3 @@ def contact(request):
     # Content from request or database extracted here 
     # and passed to the template for display
     return render(request,'about/contact.html')
-
-
--- Django_2_2/1_introduction_to_django/coffeehouse/urls.py
+++ Django_2_2/1_introduction_to_django/coffeehouse/urls.py
@@ -1,7 +1,7 @@
 """coffeehouse URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/dev/topics/http/urls/
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,13 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include 
+from django.urls import path, include
 from django.views.generic import TemplateView
 from coffeehouse.about import views as about_views
 
 urlpatterns = [
+    path('about/', about_views.contact),    
     path('admin/', admin.site.urls),
-    path('admin/doc/', include('django.contrib.admindocs.urls')),    
+    path('admin/doc/', include('django.contrib.admindocs.urls')),
     path('',TemplateView.as_view(template_name='homepage.html')),
-    path('about/', about_views.contact),
 ]
