6b2c70a31e72515f2512be4247cce151d05063e9
Daniel R <daniel@webforefront.com>
2021-04-19 22:18:25 -0500

    Removed Django 2 2

--- Django_2_2/1_introduction_to_django/coffeehouse/about/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-def contact(request):
-    # Content from request or database extracted here 
-    # and passed to the template for display
-    return render(request,'about/contact.html')
